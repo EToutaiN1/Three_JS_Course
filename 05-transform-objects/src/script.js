d72dbbce5b20180006d3da93f6bdeab0d38afd8c
EToutaiN1 <toutain.ewan@gmail.com>
2022-12-20 17:26:32 +0100

    modified:   05-transform-objects/src/script.js 	new file:   06-animations/bundler/webpack.common.js 	new file:   06-animations/bundler/webpack.dev.js 	new file:   06-animations/bundler/webpack.prod.js 	new file:   06-animations/package-lock.json 	new file:   06-animations/package.json 	new file:   06-animations/readme.md 	new file:   06-animations/src/index.html 	new file:   06-animations/src/script.js 	new file:   06-animations/src/style.css 	new file:   06-animations/static/.gitkeep

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -10,10 +10,66 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0x0000ff })
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
+
+// // Position
+
+// // mesh.position.x = 0.7
+// // mesh.position.y = - 0.6
+// // mesh.position.z = 1
+// mesh.position.set(0.7, -0.6, 1)
+
+
+// // Scale
+
+// // mesh.scale.x = 2
+// // mesh.scale.y = 0.25
+// // mesh.scale.z = 0.5
+// mesh.scale.set(2, 0.25, 0.5)
+
+// // Rotation
+
+// mesh.rotation.reorder('YXZ')
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
+
+ const group = new THREE.Group()
+ group.scale.y = 2
+ group.rotation.y = 0.2
+ scene.add(group)
+ 
+ const cube1 = new THREE.Mesh(
+     new THREE.BoxGeometry(1, 1, 1),
+     new THREE.MeshBasicMaterial({ color: 0xff0000 })
+ )
+ cube1.position.x = - 1.5
+ group.add(cube1)
+ 
+ const cube2 = new THREE.Mesh(
+     new THREE.BoxGeometry(1, 1, 1),
+     new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+ )
+ cube2.position.x = 0
+ group.add(cube2)
+ 
+ const cube3 = new THREE.Mesh(
+     new THREE.BoxGeometry(1, 1, 1),
+     new THREE.MeshBasicMaterial({ color: 0x0000ff })
+ )
+ cube3.position.x = 1.5
+ group.add(cube3)
+
+// Axes Helper
+
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+// mesh.position.normalize()
 
 /**
  * Sizes
@@ -29,6 +85,10 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+// camera.lookAt(mesh.position)
+
+// console.log(mesh.position.distanceTo(camera.position))
 
 /**
  * Renderer

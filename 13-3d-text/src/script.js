5314d7169be4529f4e12293012d8f654ca1a6caa
EToutaiN1 <toutain.ewan@gmail.com>
2023-01-02 22:56:00 +0100

    13-3D-text

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -2,6 +2,10 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+// import typefaceFont from 'three/examples/fonts/helvetiker_regular.typeface.json'
+import {FontLoader} from 'three/examples/jsm/loaders/FontLoader.js'
+import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry.js'
+
 
 /**
  * Base
@@ -15,20 +19,59 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Axes Helper
+
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper)
+
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
 
+const firstTexture = textureLoader.load('textures/matcaps/6.png')
+
+/**
+ * Fonts
+ */
+
+const fontLoader = new FontLoader()
+
+fontLoader.load('fonts/helvetiker_regular.typeface.json',
+(font) => {
+    console.log('loaded');
+
+    const textGeometry = new TextGeometry(
+        'Hello Three.js',
+        {
+            font: font,
+            size: 0.5,
+            height: 0.2,
+            curveSegments: 5,
+            bevelEnabled: true,
+            bevelThickness: 0.03,
+            bevelSize: 0.02,
+            bevelOffset: 0,
+            bevelSegments: 4
+        }
+    )
+    const textMaterial = new THREE.MeshMatcapMaterial()
+    textMaterial.wireframe = true
+    const text = new THREE.Mesh(textGeometry, textMaterial)
+    scene.add(text)
+})
+
+
+
 /**
  * Object
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial()
+// )
 
-scene.add(cube)
+// scene.add(cube)
 
 /**
  * Sizes

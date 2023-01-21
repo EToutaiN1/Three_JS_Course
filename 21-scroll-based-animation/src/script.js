8435e07b0ffd2df3ed738eff1b949beca275609b
EToutaiN1 <toutain.ewan@gmail.com>
2023-01-21 23:12:51 +0100

    Update script.js

--- 21-scroll-based-animation/src/script.js
+++ 21-scroll-based-animation/src/script.js
@@ -36,7 +36,7 @@ const scene = new THREE.Scene()
 // Texture
 const textureLoader = new THREE.TextureLoader()
 const gradientTexture = textureLoader.load('textures/gradients/3.jpg')
-gradientTexture.magFilter = THREE.NearestFilter
+// gradientTexture.magFilter = THREE.NearestFilter
 
 // Material
 
